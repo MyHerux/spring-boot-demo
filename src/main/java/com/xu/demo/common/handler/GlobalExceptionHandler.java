e5d78fd6e9f14fcea28c8bb497e5557703a84722
myherux <heroxu123@gmail.com>
2017-06-07 20:35:47 +0800

    druid add & 统一异常处理 add & yaml add

--- src/main/java/com/xu/demo/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/xu/demo/common/handler/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -18,7 +17,7 @@ public class GlobalExceptionHandler {
     private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = BusinessException.class)
-    public JSONObject BusinessExceptionHandler(HttpServletRequest request, BusinessException exception) throws IOException {
+    public JSONObject BusinessExceptionHandler(BusinessException exception) throws IOException {
         logger.info(exception.toString());
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("response_code", exception.getCode());
@@ -27,7 +26,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(value = Exception.class)
-    public JSONObject OtherExceptionHandler(HttpServletRequest request, Exception e) throws IOException {
+    public JSONObject OtherExceptionHandler(Exception e) throws IOException {
         logger.error(e.toString());
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("response_code", "300005");
